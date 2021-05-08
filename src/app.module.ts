c226f89ed102ff21e1f7e8b374df000b93153f8e
Helton Ricardo <50843386+heltonr13@users.noreply.github.com>
2021-05-08 13:24:22 -0300

    Revert "alteracao do controller produtos para produto"
    This reverts commit 3d597113c3570ee9318995c9047bcc7c7a5c4d38.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosController } from './produto.controller';
+import { ProdutosController } from './produtos.controller';
 
 @Module({
   imports: [],
