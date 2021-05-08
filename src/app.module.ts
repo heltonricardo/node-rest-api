3d597113c3570ee9318995c9047bcc7c7a5c4d38
Helton Ricardo <50843386+heltonr13@users.noreply.github.com>
2021-05-08 13:22:14 -0300

    alteracao do controller produtos para produto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosController } from './produtos.controller';
+import { ProdutosController } from './produto.controller';
 
 @Module({
   imports: [],
---
+++ src/produto.model.ts
@@ -0,0 +1,10 @@
+interface Produto {
+  id: number,
+  codigo: string,
+  nome: string,
+  preco_venda: 
+}
+
+export class Produto {
+
+}
